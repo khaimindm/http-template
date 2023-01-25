dc27f00019d9c9ed1d601be05c9e3727d54919ee
Dmitrii Khaimin <khaimindm@gmail.com>
2023-01-25 21:37:27 +0400

    25.01.23_2

--- src/main/java/com/epam/izh/rd/online/Http.java
+++ src/main/java/com/epam/izh/rd/online/Http.java
@@ -1,18 +1,20 @@
 package com.epam.izh.rd.online;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 public class Http {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://pokeapi.co/api/v2/";
+        String url = "https://pokeapi.co/api/v2/pokemon/ditto/";
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("User-Agent", "");
-        
-        String response = restTemplate.getForObject(url, String.class);
-        System.out.println(response);
+        headers.set("User-Agent", "");
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        System.out.println(response.getBody());
     }
 }
