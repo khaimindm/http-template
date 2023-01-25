142c58bac881eaa27ad764125c6d3264f25bb62f
khaimindm <khaimindm@gmail.com>
2023-01-25 11:48:00 +0000

    25.02.23

--- src/main/java/com/epam/izh/rd/online/Http.java
+++ src/main/java/com/epam/izh/rd/online/Http.java
@@ -1,7 +1,18 @@
 package com.epam.izh.rd.online;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
 public class Http {
     public static void main(String[] args) {
-
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://pokeapi.co/api/v2/";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("User-Agent", "");
+        
+        String response = restTemplate.getForObject(url, String.class);
+        System.out.println(response);
     }
 }
