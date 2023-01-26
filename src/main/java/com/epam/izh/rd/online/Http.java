2fd13c4f165f45a97fd27779634fbd8805b861fa
khaimindm <khaimindm@gmail.com>
2023-01-26 12:04:37 +0000

    26.01.23

--- src/main/java/com/epam/izh/rd/online/Http.java
+++ src/main/java/com/epam/izh/rd/online/Http.java
@@ -4,17 +4,34 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
+import java.util.Scanner;
 
 public class Http {
     public static void main(String[] args) {
+        System.out.println("Введите имя первого покемона (например: pikachu, ditto... Список всех имен можно найти на https://pokeapi.co/):");
+        Scanner scanner = new Scanner(System.in);
+        String nameOfFirstPokemon = scanner.nextLine();
+
+        System.out.println("Введите имя второго покемона (например: pikachu, ditto... Список всех имен можно найти на https://pokeapi.co/):");
+        String nameOfSecondPokemon = scanner.nextLine();
+
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://pokeapi.co/api/v2/pokemon/ditto/";
+        String urlFirstPokemon = "https://pokeapi.co/api/v2/pokemon/" + nameOfFirstPokemon;
+        String urlSecondPokemon = "https://pokeapi.co/api/v2/pokemon/" + nameOfSecondPokemon;
         HttpHeaders headers = new HttpHeaders();
         headers.set("User-Agent", "");
 
         HttpEntity<String> request = new HttpEntity<>(headers);
 
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
-        System.out.println(response.getBody());
+        ResponseEntity<String> responseFirstPokemon = restTemplate.exchange(urlFirstPokemon, HttpMethod.GET, request, String.class);
+        ResponseEntity<String> responseSecondPokemon = restTemplate.exchange(urlSecondPokemon, HttpMethod.GET, request, String.class);
+        
+        System.out.println("Первый покемон");
+        System.out.println(responseFirstPokemon.getBody());
+
+        System.out.println("Второй покемон");
+        System.out.println(responseSecondPokemon.getBody());
+
+        scanner.close();
     }
 }
