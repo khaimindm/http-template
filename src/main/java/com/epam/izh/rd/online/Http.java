371c045210a3b2227b579acd039189a06f32775a
khaimindm <khaimindm@gmail.com>
2023-02-16 11:35:44 +0000

    16.02.23_Finish

--- src/main/java/com/epam/izh/rd/online/Http.java
+++ src/main/java/com/epam/izh/rd/online/Http.java
@@ -7,28 +7,10 @@ import com.epam.izh.rd.online.service.PokemonFetchingService;
 import com.epam.izh.rd.online.service.PokemonFightingClubService;
 import com.epam.izh.rd.online.service.SimplePokemonFetchingService;
 import com.epam.izh.rd.online.service.SimplePokemonFightingClubService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
-
 import java.io.IOException;
-import java.util.Map;
-import java.util.Scanner;
 
 public class Http {
     public static void main(String[] args) throws IOException {
-        //RestTemplate restTemplate = new RestTemplate();
-        //String urlFirstPokemon = "https://pokeapi.co/api/v2/pokemon/" + nameOfFirstPokemon;
-        //String urlSecondPokemon = "https://pokeapi.co/api/v2/pokemon/" + nameOfSecondPokemon;
-        //HttpHeaders headers = new HttpHeaders();
-        //headers.set("User-Agent", "");
-
-        //HttpEntity<String> request = new HttpEntity<>(headers);
-
-        //ResponseEntity<String> responseFirstPokemon = restTemplate.exchange(urlFirstPokemon, HttpMethod.GET, request, String.class);
-        //ResponseEntity<String> responseSecondPokemon = restTemplate.exchange(urlSecondPokemon, HttpMethod.GET, request, String.class);
-
-        
         String pokemonName1 = "pikachu";
         String pokemonName2 = "slowpoke";
 
--- src/main/java/com/epam/izh/rd/online/service/Stats.java
+++ src/main/java/com/epam/izh/rd/online/service/Stats.java
@@ -1,7 +1,5 @@
 package com.epam.izh.rd.online.service;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
